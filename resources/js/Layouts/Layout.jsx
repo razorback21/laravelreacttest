4dd890e1a03ee89fedb06bce7b3849e9cd9f6a4f
Donz <donnygapulao@gmail.com>
2025-08-19 00:51:42 +0800

    removed extra styling

--- resources/js/Layouts/Layout.jsx
+++ resources/js/Layouts/Layout.jsx
@@ -7,7 +7,7 @@ export default function Layout({ title = "Laravel 8 React", children }) {
             <Head title={title} />
             <AppNavigation />
             <div className="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
-                <div className="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md sm:rounded-lg">
+                <div className="w-full sm:max-w-md mt-6 px-6 py-4  sm:rounded-lg">
                     {children}
                 </div>
             </div>
