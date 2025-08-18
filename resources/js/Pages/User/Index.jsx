268b04497d7eca1031049ad1d482a85171099e08
Donz <donnygapulao@gmail.com>
2025-08-18 23:00:31 +0800

    moved users table to a separate component

--- resources/js/Pages/User/Index.jsx
+++ resources/js/Pages/User/Index.jsx
@@ -1,10 +1,28 @@
 import Layout from "@/Layouts/Layout";
+import UsersTable from "./UsersTable";
+import { Link } from "@inertiajs/inertia-react";
 
 export default function Index({ users }) {
+    console.log(users);
     return (
         <Layout title="Users">
             <h1 className="font-bold mb-5">Users</h1>
             <div className="form-control w-full"></div>
+            {!users.length ? (
+                <div>
+                    <p className="text-center text-sm bg-blue-200 border border-blue-300 mb-2 p-2 rounded-md text-blue-900">
+                        There are no users in the database
+                    </p>
+
+                    <Link href={route("users.create")}>
+                        <button className="btn btn-neutral w-full">
+                            Create User
+                        </button>
+                    </Link>
+                </div>
+            ) : (
+                <UsersTable users={users} />
+            )}
         </Layout>
     );
 }
---
+++ resources/js/Pages/User/UsersTable.jsx
@@ -0,0 +1,22 @@
+export default function UsersTable({ users }) {
+    return (
+        <table className="table table-zebra w-full">
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>Email</th>
+                    <th>Roles</th>
+                </tr>
+            </thead>
+            <tbody>
+                {users.map((user) => (
+                    <tr key={user.id}>
+                        <td>{user.id}</td>
+                        <td>{user.email}</td>
+                        <td>{user.roles}</td>
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    );
+}
