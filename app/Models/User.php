f8ec1fa9186b4060099d62859ba857ff9ae8ebd8
Donz <donnygapulao@gmail.com>
2025-08-19 00:35:40 +0800

    added belongtoMany roles relation

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,12 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    /**
+     * Get the roles associated with the user.
+     */
+    public function roles()
+    {
+        return $this->belongsToMany(Role::class);
+    }
 }
