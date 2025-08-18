720654518003320362216ba93d97fa732a3e40d3
Donz <donnygapulao@gmail.com>
2025-08-18 21:19:09 +0800

    installed daisy ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,21 +1,46 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './resources/js/**/*.jsx',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./resources/js/**/*.jsx",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
+                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
             },
         },
     },
+    daisyui: {
+        themes: [
+            {
+                mytheme: {
+                    primary: "#570df8",
 
-    plugins: [require('@tailwindcss/forms')],
+                    secondary: "#f000b8",
+
+                    accent: "#1dcdbc",
+
+                    neutral: "#2b3440",
+
+                    "base-100": "#ffffff",
+
+                    info: "#3abff8",
+
+                    success: "#36d399",
+
+                    warning: "#fbbd23",
+
+                    error: "#f87272",
+                },
+            },
+        ],
+    },
+
+    plugins: [require("@tailwindcss/forms"), require("daisyui")],
 };
